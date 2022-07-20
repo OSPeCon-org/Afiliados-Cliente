3a9666c0a1dd56193f2ac3ef13389471f33e7b30
Daniel Di Lorenzo <ddilorenzo@gmail.com>
2022-07-20 11:44:26 -0300

    tarjeta familia

--- src/views/manager.js
+++ src/views/manager.js
@@ -13,98 +13,102 @@ import { gridLayout } from "@brunomon/template-lit/src/views/css/gridLayout";
 
 import { pruebaTarjeta } from "./componentes/pruebaTarjeta";
 import { pruebaTarjetaDocumento } from "./componentes/pruebaTarjetaDocumento";
+import { pruebaTarjetaFamilia } from "./prueba/pruebaTarjetaFamilia";
 
 const MEDIA_CHANGE = "ui.media.timeStamp";
 const SCREEN = "screen.timeStamp";
 const SELECTION = "ui.menu.timeStamp";
 
 export class viewManager extends connect(store, MEDIA_CHANGE, SCREEN, SELECTION)(LitElement) {
-    constructor() {
-        super();
-        window.onpopstate = (event) => {
-            if (event.state) {
-                store.dispatch(goTo(event.state.option, true));
-            } else {
-                window.history.back();
-            }
-        };
-    }
+	constructor() {
+		super();
+		window.onpopstate = (event) => {
+			if (event.state) {
+				store.dispatch(goTo(event.state.option, true));
+			} else {
+				window.history.back();
+			}
+		};
+	}
 
-    static get styles() {
-        return css`
-            ${layoutsCSS}
-            ${gridLayout}
+	static get styles() {
+		return css`
+			${layoutsCSS}
+			${gridLayout}
             ${spinner}
             :host {
-                display: grid;
-                padding: 0;
-                background-color: var(--aplicacion);
-                overflow: hidden;
-            }
+				display: grid;
+				padding: 0;
+				background-color: var(--aplicacion);
+				overflow: hidden;
+			}
 
-            :host::-webkit-scrollbar {
-                width: 0.5vw;
-                cursor: pointer;
-            }
-            :host::-webkit-scrollbar([media-size="small"]) {
-                display: none;
-            }
-            :host::-webkit-scrollbar-thumb {
-                background: var(--secundario);
-                border-radius: 5px;
-            }
-        `;
-    }
+			:host::-webkit-scrollbar {
+				width: 0.5vw;
+				cursor: pointer;
+			}
+			:host::-webkit-scrollbar([media-size="small"]) {
+				display: none;
+			}
+			:host::-webkit-scrollbar-thumb {
+				background: var(--secundario);
+				border-radius: 5px;
+			}
+		`;
+	}
 
-    render() {
-        return html`
-            <div class="spinner" anillo fixed hidden></div>
-            <menu-principal area="header"></menu-principal>
-            <!--
+	render() {
+		return html`
+			<div class="spinner" anillo fixed hidden></div>
+			<menu-principal area="header"></menu-principal>
+			<!--
             <form-test area="body"></form-test>>
             <prueba-tarjeta></prueba-tarjeta>
             -->
-            <prueba-tarjetadocumento></prueba-tarjetadocumento>
-        `;
-    }
+			<!--
+				<prueba-tarjetadocumento></prueba-tarjetadocumento>
+            -->
+			<prueba-tarjeta-familia id="pruebaTarjetaFamilia" area="body"></prueba-tarjeta-familia>
+		`;
+	}
 
-    stateChanged(state, name) {
-        if (name == MEDIA_CHANGE || name == SCREEN) {
-            this.mediaSize = state.ui.media.size;
-            this.orientation = state.ui.media.orientation;
-            this.layout = getLayout(state).name;
-            if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
-                if ("standalone" in window.navigator && window.navigator.standalone) {
-                    this.style.height = document.documentElement.offsetHeight ? document.documentElement.offsetHeight : window.innerHeight + "px";
-                } else {
-                    if (state.ui.media.orientation == "portrait") {
-                        this.style.height = window.innerHeight < window.innerWidth ? window.innerWidth : window.innerHeight + "px";
-                    } else {
-                        this.style.height = window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight + "px";
-                    }
-                }
-            }
-        }
-        this.update();
-    }
+	stateChanged(state, name) {
+		if (name == MEDIA_CHANGE || name == SCREEN) {
+			this.mediaSize = state.ui.media.size;
+			this.orientation = state.ui.media.orientation;
+			this.layout = getLayout(state).name;
+			if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
+				if ("standalone" in window.navigator && window.navigator.standalone) {
+					this.style.height = document.documentElement.offsetHeight ? document.documentElement.offsetHeight : window.innerHeight + "px";
+				} else {
+					if (state.ui.media.orientation == "portrait") {
+						this.style.height = window.innerHeight < window.innerWidth ? window.innerWidth : window.innerHeight + "px";
+					} else {
+						this.style.height = window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight + "px";
+					}
+				}
+			}
+		}
+		this.update();
+	}
 
-    static get properties() {
-        return {
-            mediaSize: {
-                type: String,
-                reflect: true,
-                attribute: "media-size",
-            },
-            layout: {
-                type: String,
-                reflect: true,
-            },
-            orientation: {
-                type: String,
-                reflect: true,
-            },
-        };
-    }
+	static get properties() {
+		return {
+			mediaSize: {
+				type: String,
+				reflect: true,
+				attribute: "media-size",
+			},
+			layout: {
+				type: String,
+				reflect: true,
+			},
+			orientation: {
+				type: String,
+				reflect: true,
+			},
+		};
+	}
 }
 
 window.customElements.define("view-manager", viewManager);
---
+++ src/views/prueba/pruebaTarjetaFamilia.js
@@ -0,0 +1,114 @@
+/** @format */
+
+import { html, LitElement, css } from "lit";
+import { store } from "../../redux/store";
+import { connect } from "@brunomon/helpers";
+
+import { BENEF, GRPFAM, ADD, MODIF, VER } from "../../../assets/icons/svgs.js";
+import { gridLayout } from "@brunomon/template-lit/src/views/css/gridLayout";
+import { isInLayout } from "../../redux/screens/screenLayouts";
+import { button } from "@brunomon/template-lit/src/views/css/button";
+import { tarjetaFamilia } from "../css/tarjetaFamilia";
+import { tarjetaPersona } from "../css/tarjetaPersona";
+
+const MEDIA_CHANGE = "ui.media.timeStamp";
+const SCREEN = "screen.timeStamp";
+
+export class pruebaTarjetaFamilia extends connect(store, SCREEN, MEDIA_CHANGE)(LitElement) {
+	constructor() {
+		super();
+		this.items = [
+			{ icono: "BENEF", parentesco: "Titular", leyenda: "Dar de alta al titular del grupo familiar", boton: "AGREGAR" },
+			{ icono: "GRPFAM", parentesco: "Grupo Familiar", leyenda: "Dar de alta un nuevo integante del grupo familiar", boton: "AGREGAR" },
+		];
+		this.items1 = [
+			{ icono: "VER", parentesco: "Titular", imagen: "", nombre: "Jose Perez", estado: "Afiliacion activa" },
+			{ icono: "MODIF", parentesco: "Conyuje", imagen: "", nombre: "Juan Jose Ruiz", estado: "Afiliacion activa" },
+			{ icono: "", parentesco: "Hijo", imagen: "", nombre: "Juan Jose Ruiz", estado: "Afiliacion activa" },
+		];
+
+		this.area = "body";
+		this.svgs = { VER: VER, BENEF: BENEF, GRPFAM: GRPFAM, ADD: ADD, MODIF: MODIF };
+	}
+
+	static get styles() {
+		return css`
+			${gridLayout}
+			${tarjetaFamilia}
+			${tarjetaPersona}
+            ${button}
+            :host {
+				display: grid;
+				grid-template-rows: auto 1fr;
+			}
+			.grilla {
+				overflow-y: auto;
+			}
+		`;
+	}
+
+	render() {
+		return html`
+			<div class="grid row grilla itemsCenter" style="grid-gap:2rem">
+				${this.items.map((item) => {
+					return html`
+						<div class="tarjeta-familia">
+							<div titulo>
+								<div help>${this.svgs[item.icono]}</div>
+								<div>${item.parentesco}</div>
+							</div>
+							<div cuerpo>${item.leyenda}</div>
+							<button raised>${item.boton}</button>
+						</div>
+					`;
+				})}
+				${this.items1.map((item) => {
+					return html`
+						<div class="tarjeta-persona">
+							<div titulo>
+								<div help style="visibility:${item.icono == "" ? "hidden" : "visible"};" @click="${this.icono}">${this.svgs[item.icono]}</div>
+								<div>${item.parentesco}</div>
+							</div>
+							<div cuerpo><img src="https://app.uocra.org/credencialSindical/sinusuario.png" /></div>
+							<div nombre>${item.nombre}</div>
+							<div estado>${item.estado}</div>
+						</div>
+					`;
+				})}
+			</div>
+		`;
+	}
+
+	firstUpdated(changedProperties) {}
+
+	stateChanged(state, name) {
+		if (name == MEDIA_CHANGE) {
+			this.mediaSize = state.ui.media.size;
+		}
+		if (name == SCREEN) {
+			this.hidden = true;
+			const isCurrentScreen = ["pruebaTarjetaFamilia"].includes(state.screen.name);
+			if (isInLayout(state, this.area) && isCurrentScreen) {
+				this.hidden = false;
+			}
+		}
+	}
+	icono() {
+		alert("Hola");
+	}
+	static get properties() {
+		return {
+			mediaSize: {
+				type: String,
+				reflect: true,
+				attribute: "media-size",
+			},
+			icono: {
+				type: String,
+				reflect: true,
+			},
+		};
+	}
+}
+
+window.customElements.define("prueba-tarjeta-familia", pruebaTarjetaFamilia);
