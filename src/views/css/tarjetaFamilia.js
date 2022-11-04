c99f62326ec8a387b209fb571c52722aed7e67f6
FlorenciaPenaranda <florenciaceciliap@gmail.com>
2022-11-04 16:17:43 -0300

    paresntesco + tarjetaMostrar

--- src/views/css/tarjetaFamilia.js
+++ src/views/css/tarjetaFamilia.js
@@ -12,59 +12,59 @@
 import { css } from "lit-element";
 
 export const tarjetaFamilia = css`
-	.tarjeta-familia {
-		display: grid;
-		width: 12rem;
-		height: 9rem;
-		background-color: var(--formulario);
-		gap: 0;
-		padding: 0;
-		grid-template-rows: 3.5fr 8fr 3fr;
-		align-content: start;
-		border-radius: 5px;
-		overflow: hidden;
-	}
+    .tarjeta-familia {
+        display: grid;
+        width: 15rem;
+        height: 11rem;
+        background-color: var(--formulario);
+        gap: 0;
+        padding: 0;
+        grid-template-rows: 3.5fr 8fr 3fr;
+        align-content: start;
+        border-radius: 5px;
+        overflow: hidden;
+    }
 
-	.tarjeta-familia div[titulo] {
-		display: grid;
-		grid-template-columns: 1fr auto;
-		color: var(--primario);
-		font-family: var(--font-header-h1-menos-family);
-		font-size: var(--font-header-h1-menos-size);
-		font-weight: var(--font-header-h1-weight);
-		padding: 0 0.4rem;
-		align-content: center;
-		border-bottom: solid 1px var(--on-aplicacion-separador);
-	}
-	.tarjeta-familia div[help] {
-		display: grid;
-	}
+    .tarjeta-familia div[titulo] {
+        display: grid;
+        grid-template-columns: 1fr auto;
+        color: var(--primario);
+        font-family: var(--font-header-h1-menos-family);
+        font-size: var(--font-header-h1-menos-size);
+        font-weight: var(--font-header-h1-weight);
+        padding: 0 0.4rem;
+        align-content: center;
+        border-bottom: solid 1px var(--on-aplicacion-separador);
+    }
+    .tarjeta-familia div[help] {
+        display: grid;
+    }
 
-	.tarjeta-familia div[titulo] svg {
-		width: 2rem;
-		height: 2rem;
-		fill: var(--on-formulario-bajada) !important;
-		opacity: 0.5;
-	}
-	.tarjeta-familia div[cuerpo] {
-		font-family: var(--font-header-h1-menos-family);
-		font-size: var(--font-header-h1-menos-size);
-		font-weight: var(--font-header-h1-menos-weight);
-		color: var(--on-formulario-bajada);
-		margin: auto;
-		font-size: 1rem;
-		padding: 0 0.4rem;
-		opacity: 0.7;
-	}
-	.tarjeta-familia button {
-		width: 100%;
-	}
-	.tarjeta-familia button {
-		width: 100%;
-	}
-	.tarjeta-familia button[raised] {
-		box-shadow: none !important;
-	}
-	.tarjeta-familia[dark] {
-	}
+    .tarjeta-familia div[titulo] svg {
+        width: 2rem;
+        height: 2rem;
+        fill: var(--on-formulario-bajada) !important;
+        opacity: 0.5;
+    }
+    .tarjeta-familia div[cuerpo] {
+        font-family: var(--font-header-h1-menos-family);
+        font-size: var(--font-header-h1-menos-size);
+        font-weight: var(--font-header-h1-menos-weight);
+        color: var(--on-formulario-bajada);
+        margin: auto;
+        font-size: 1rem;
+        padding: 0 0.4rem;
+        opacity: 0.7;
+    }
+    .tarjeta-familia button {
+        width: 100%;
+    }
+    .tarjeta-familia button {
+        width: 100%;
+    }
+    .tarjeta-familia button[raised] {
+        box-shadow: none !important;
+    }
+    .tarjeta-familia[dark] {
+    }
 `;
